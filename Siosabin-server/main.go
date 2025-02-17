174b2eb57281e671679795aa44a4d469cdc6f093
Shutaro Kawamitsu <assam5649.18@gmail.com>
2025-02-17 21:26:15 +0900

    Replace some folder

---
+++ Siosabin-server/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/assam5649/Siosabin-go/Siosabin-server/pkg/config"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+
+	config.Wait()
+
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hello, Go Server!",
+		})
+	})
+
+	port := "5000"
+	fmt.Println("Starting Gin server on port ", port)
+
+	if err := r.Run(port); err != nil {
+		log.Fatal("Failed to start server:", err)
+	}
+}
